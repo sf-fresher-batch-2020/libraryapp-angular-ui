aa9d2b8809c962606c747451568c66440ad5b7c9
PasamPavithra <pasampavithra27@gmail.com>
2020-11-17 21:44:33 +0530

    update register

--- projects/frontend/src/app/app.module.ts
+++ projects/frontend/src/app/app.module.ts
@@ -10,8 +10,9 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ToastrModule } from 'ngx-toastr';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { RegisterComponent } from './components/register/register.component';
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, RegisterComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
