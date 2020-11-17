681af69b0211d65cbd9295782434fd0038b45dce
PasamPavithra <pasampavithra27@gmail.com>
2020-11-17 13:20:50 +0530

    Updated Header

--- projects/frontend/src/app/components/header/header.component.ts
+++ projects/frontend/src/app/components/header/header.component.ts
@@ -7,15 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  loggedInUser;
 
   constructor(
     
   ) { }
 
   ngOnInit(): void {
-    this.loggedInUser =  localStorage.getItem("LOGGED_IN_USER") != null ? true: false;
+    
   }
 
-  
+  logout(){}
 }
